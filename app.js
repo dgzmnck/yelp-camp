63159d6b4d0d970f362dd168b5d3b0babd8aecba
dgzmnck <developernicko@gmail.com>
2022-06-29 16:36:29 +0800

    fixing errors session current user

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ store.on("error", function (e) {
 //app.use(session({}));
 app.use(
   session({
-    store: MongoStore.create({ mongoUrl: "mongodb://localhost/test-app" }),
+    store: MongoStore.create({ mongoUrl: db_url, touchAfter: 24 * 60 * 60 }),
     name: "shesh",
     resave: false,
     saveUninitialized: true,
